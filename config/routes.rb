18c512ece3f4a79eada0945551422817c9b043ce
Raphael Molesim <raphael.molesim@gmail.com>
2018-03-04 23:24:35 -0300

    Encerramento de inscrições

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'admin/surveys'
 
-  resources :surveys, only: [:new, :create ]
+  #resources :surveys, only: [:new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'papers/:id', to: "papers#show"
