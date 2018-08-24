def0b627a8d332bc0bff1f2079270a497191cce2
Raphael Molesim <raphael.molesim@gmail.com>
2018-08-24 14:01:55 -0300

    starting ACC 2019 web site

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,19 +1,29 @@
 class WelcomeController < ApplicationController
 
+  def index
+    render_based_by_year :index
+  end
+
   def payment
-    
+    render_based_by_year :payment
   end
   
   def advertising
-    
+    render_based_by_year :advertising
   end
   
   def prestacao_contas
-    
+    render_based_by_year :prestacao_contas
   end
   
   def conteudo_gerado
-    
+    render_based_by_year :conteudo_gerado
   end
+  
+  private 
+    def render_based_by_year action
+      year = params[:year]
+      render "#{year}/welcome/#{action}", layout: "#{year}/layouts/application.html.erb"
+    end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
 
   #resources :surveys, only: [:new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'welcome#index', :year => 2019
   
   scope "/2018" do
-    root 'welcome#index'
-    get 'papers/:id', to: "papers#show"
-    get 'surveys', to: "admin#surveys"
-    get 'advertising', to: "welcome#advertising"
-    get 'prestacao_contas', to: "welcome#prestacao_contas"
-    get 'conteudo_gerado', to: "welcome#conteudo_gerado"
+    root 'welcome#index', :year => 2018
+    get 'papers/:id', to: "papers#show", :year => 2018
+    get 'surveys', to: "admin#surveys", :year => 2018
+    get 'advertising', to: "welcome#advertising", :year => 2018
+    get 'prestacao_contas', to: "welcome#prestacao_contas", :year => 2018
+    get 'conteudo_gerado', to: "welcome#conteudo_gerado", :year => 2018
   end
   
 end
