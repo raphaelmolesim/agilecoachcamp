8677b93dbee813f78361c30d93b89d5c554e9799
Raphael Molesim <raphael.molesim@gmail.com>
2018-12-10 11:16:14 -0200

    Position Paper was not working

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,7 +1,13 @@
 class PapersController < ApplicationController
   
   def show
-    render "#{params[:id]}.html.erb"
+    render_based_by_year "#{params[:id]}.html.erb"
   end
+  
+  private 
+    def render_based_by_year action
+      year = params[:year]
+      render "#{year}/papers/#{action}", layout: "#{year}/layouts/application.html.erb"
+    end
   
 end
